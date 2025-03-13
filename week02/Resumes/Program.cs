c14f65ee02f97c7a07d35a3f4fe2db3203ce329a
Benjamin Ruiz <bruiz@popoyan.com>
2025-03-12 23:43:04 -0600

    Resume activity

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job firstJob = new()
+        {
+            _jobTitle = "Software Developer",
+            _company = "IBM",
+            _startYear = 2016,
+            _endYear = 2020
+        };
+
+        Job secondJob = new()
+        {
+            _jobTitle = "Software Architech",
+            _company = "Microsoft",
+            _startYear = 2021,
+            _endYear = 2024
+        };
+
+        Job thirdJob = new()
+        {
+            _jobTitle = "Software Engineer",
+            _company = "Google",
+            _startYear = 2021,
+            _endYear = 2024
+        };
+
+        Resume myResume = new()
+        {
+            _name = "Benjamin Ruiz"
+        };
+
+        myResume._jobs.Add(firstJob);
+        myResume._jobs.Add(secondJob);
+        myResume._jobs.Add(thirdJob);
+
+        myResume.Display();
     }
 }
