1ea1b5ab93ccbd0817da7cae3e05841fc1ac99ce
Benjamin Ruiz <bruiz@popoyan.com>
2025-03-29 01:05:43 -0600

    Week four Youtube Video Project

---
+++ week04/YouTubeVideos/VIdeo.cs
@@ -0,0 +1,45 @@
+public class Video
+{
+    private string _title;
+    private string _author;
+    private int _lengthInSeconds;
+    private List<Comment> _comments;
+
+    public Video(string title, string author, int lengthInSeconds)
+    {
+        _title = title;
+        _author = author;
+        _lengthInSeconds = lengthInSeconds;
+        _comments = new List<Comment>();
+    }
+
+    public void AddComment(Comment comment)
+    {
+        _comments.Add(comment);
+    }
+
+    public int GetCommentCount()
+    {
+        return _comments.Count;
+    }
+
+    public List<Comment> GetComments()
+    {
+        return _comments;
+    }
+
+    public void DisplayVideoInfo()
+    {
+        Console.WriteLine($"Title: {_title}");
+        Console.WriteLine($"Author: {_author}");
+        Console.WriteLine($"Length: {_lengthInSeconds} seconds");
+        Console.WriteLine($"Comments: {GetCommentCount()}");
+
+        foreach (Comment comment in _comments)
+        {
+            Console.WriteLine($"- {comment.GetCommenterName()}: {comment.GetCommentText()}");
+        }
+
+        Console.WriteLine();
+    }
+}
